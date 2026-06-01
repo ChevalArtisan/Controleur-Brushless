d47719ff54bd10f2f76be8ec35c3d7f8a404b26f
ChevalArtisan <hippolyte.handwerk@gmail.com>
2026-06-01 15:06:01 +0200

    Fix : ajout signaux tour

--- test_BLDC.sv
+++ test_BLDC.sv
@@ -1,15 +1,24 @@
-module test_vitesse;
+module test_BLDC;
 	
-	logic clk, rst;
+	logic clk, rst, tour;
 	logic unsigned [7:0] duty;
-	logic unsigned [15:0] max_cpt;
-	logic unsigned [15:0] current_duty;
+	logic U,V,W,Un,Vn,Wn;
+	logic tour_complet;
+	logic unsigned [15:0] vitesse_instantanee;
 
-	vitesse_ramp test(.clk(clk),
+	controleur_bldc test(
+		.clk(clk),
 		.rst(rst),
 		.duty(duty),
-		.max_cpt(max_cpt),
-		.current_duty(current_duty));
+		.U(U),
+		.V(V),
+		.W(W),
+		.Un(Un),
+		.Vn(Vn),
+		.Wn(Wn),
+		.tour(tour),
+		.tour_complet(tour_complet),
+		.vitesse_instantanee(vitesse_instantanee));
 
 	initial clk = 0;
 	always clk = #500ns ~clk;
@@ -18,8 +27,26 @@ module test_vitesse;
 		rst = 1;
 		#1ms
 		rst = 0;
-		duty = 8'b11000000;
-		max_cpt = 16'b 0100111000100000;
+		duty = 8'b11000000; //75%
+		tour = 0;
+		#10ms
+		tour = 1;
+		#10ms
+		tour = 0;
+		#10ms
+		tour = 1;
+		#10ms
+		tour = 0;
+		#10ms
+		tour = 1;
+		#10ms
+		tour = 0;
+		#10ms
+		tour = 1;
+		#10ms
+		tour = 0;
+		#10ms
+		tour = 1;	
 		#2s
 
 
